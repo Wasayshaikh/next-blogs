81dd1f10e42cb39edd2a73772cde28d5711c6069
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-01-10 21:07:36 +0500

    working on banner section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,11 +14,12 @@ export default function Home() {
       </Head>
       <div className=' bg-gray-900 w-screen h-[calc(100vh-300px)]'>
         <div className='w-[1100px] m-auto flex  h-full' >
-          <div className='flex-1 bg-blue-500'>
-            
+          <div className='flex-1 bg-blue-500 relative'>
+          <span className='p-2 bg-green-400 block max-w-24 absolute left-0 top-0'>Popular</span>
           </div>
           <div className='w-56'>
             <div className='w-full h-2/4 bg-red-500'>
+             
 
             </div>
             <div  className='w-full h-2/4  bg-green-500'></div>
