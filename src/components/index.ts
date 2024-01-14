8e5acfc4a2b59fbb6207eca8ed0a0d60c1635298
abdul wasay <abdulwasayshaikh1999@gmail.com>
2024-01-14 23:26:04 +0500

    add latest blog component in index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export {default as Header} from "@/components/Header"
 export { default as PostSwiper} from '@/components/PostSlider'
+export { default as PopularPosts} from '@/components/PopularPosts'
