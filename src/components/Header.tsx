e95f93d23222101ea374cfdbd4f8435a254d2218
abdul wasay <abdulwasayshaikh1999@gmail.com>
2024-01-14 23:59:05 +0500

    fix the home route links tag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,10 +27,10 @@ const Header = () => {
             <header className="bg-white">
                 <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
                     <div className="flex lg:flex-1">
-                        <a href="/" className="-m-1.5 p-1.5">
+                        <Link href="/" className="-m-1.5 p-1.5">
                             <span className="sr-only">Your Company</span>
                             <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-                        </a>
+                        </Link>
                     </div>
                     <div className="flex lg:hidden">
                         <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
@@ -41,7 +41,7 @@ const Header = () => {
                     <div className="hidden lg:flex lg:gap-x-12">
 
 
-                        <a href="/" className="text-sm font-semibold leading-6 text-gray-900">Home</a>
+                        <Link href="/" className="text-sm font-semibold leading-6 text-gray-900">Home</Link>
                         {/* <div className="relative">
                             <button onClick={(e) => { toggleDropDown(e) }} type="button" className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900" aria-expanded="false">
                                 Product
