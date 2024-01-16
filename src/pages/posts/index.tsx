eb28990fc89fabb758bf31600b84a6b2f4c5e5df
abdul wasay <abdulwasayshaikh1999@gmail.com>
2024-01-17 01:38:17 +0500

    fetch data through api

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,14 +1,37 @@
 import Head from 'next/head'
-import React from 'react'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
 
+type postType = {
+  data:Array<Record<string,any>>,
+  status:string|number
+} | null
 const Posts = () => {
+  const [blogs, setBlogs] = useState<postType>(null)
+  useEffect(() => {
+    fetch("http://localhost:3000/api/get-posts")
+      .then((a) => {
+        return a.json();
+      })
+      .then((parse) => {
+        setBlogs(parse)
+      })
+  }, [])
   return (
     <>
       <Head>
         <title>Posts - Coder Space</title>
         <meta name='description' content='Blog post website for coding enthusiast ' />
       </Head>
-      <div>Posts</div>
+      <div>{blogs == null ? (<>loading</>) : (blogs.data.map(blog => (
+        <Link href={'posts/'+blog.slug} key={blog.title} className='container m-auto block '>
+          <h1 className='text-2xl bold '>{blog.title}</h1>
+          <div className='text-justify'>{blog.content.substr(0,440)}....</div>
+          <div className=' text-right'>{blog.arthur}</div>
+        </Link>
+
+      )))
+      }</div>
     </>
 
   )
