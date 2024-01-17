b6c067d1e713c9166bf3a2b9af43e11f6a9703ea
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-01-17 21:11:29 +0500

    working on the shop list section of home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,26 @@ import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import Link from 'next/link';
 import { PostSwiper, PopularPosts } from '@/components';
+import { useEffect, useState } from 'react';
 
-
+type postType = {
+  data: Array<Record<string, any>>,
+  status: string | number
+} | null
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
+  const [blogs, setBlogs] = useState<postType>(null)
+  useEffect(() => {
+    fetch("http://localhost:3000/api/get-posts")
+      .then((a) => {
+        return a.json();
+      })
+      .then((parse) => {
+        setBlogs(parse)
+      })
+  }, [])
   return (
     <>
       <Head>
@@ -16,7 +30,7 @@ export default function Home() {
         <meta name='description' content='Blog post website for coding enthusiast ' />
       </Head>
       <div className=' bg-gray-900 w-screen h-[500px] md:h-[600px] lg:h-[600px] xl:h-[calc(100vh-300px)]'>
-        <div className='xl:w-[1100px] m-auto flex  h-full' >
+        <div className='lg:container m-auto flex  h-full' >
           <div className='w-full lg:w-[70%] xxl:w-[80%] relative'>
             <span className='p-2 block max-w-24 text-white font-bold absolute left-0 top-0 z-10'>Latest</span>
             <PostSwiper />
@@ -26,6 +40,40 @@ export default function Home() {
           </div>
 
         </div>
+      </div>
+      <div className='flex flex-wrap lg:container m-auto px-4 lg:p-0'>
+        <div className='bg-green-400 w-1/3 p-4 h-fit'>
+          <div className='py-2'>
+            <div><h3 className='text-lg'>{blogs.data[0].title}</h3></div>
+            <div><p className='text-justify'>{blogs.data[0].content}</p></div>
+          </div>
+          <div className='py-2'>
+            <div><h3 className='text-lg'>{blogs.data[2].title}</h3></div>
+            <div><p className='text-justify'>{blogs.data[2].content}</p></div>
+          </div>
+
+        </div>
+        <div className='bg-blue-400 w-1/3 h-fit p-4'>
+          <div className='py-2'>
+            <div><h3 className='text-lg'>{blogs.data[0].title}</h3></div>
+            <div><p className='text-justify'>{blogs.data[0].content}</p></div>
+          </div>
+          <div className='py-2'>
+            <div><h3 className='text-lg'>{blogs.data[1].title}</h3></div>
+            <div><p className='text-justify'>{blogs.data[1].content}</p></div>
+          </div>
+        </div>
+        <div className='bg-red-400 w-1/3 h-fit p-4'>
+          <div className='py-2'>
+            <div><h3 className='text-lg'>{blogs.data[1].title}</h3></div>
+            <div><p className='text-justify'>{blogs.data[1].content}</p></div>
+          </div>
+          <div className='py-2'>
+            <div><h3 className='text-lg'>{blogs.data[2].title}</h3></div>
+            <div><p className='text-justify'>{blogs.data[2].content}</p></div>
+          </div>
+        </div>
+
       </div>
     </>
   )
