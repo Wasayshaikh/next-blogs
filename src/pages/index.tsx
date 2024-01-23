002beb2c207e20edece1732b2df03f8c5a4be68e
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-01-23 19:52:16 +0500

    created post card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,10 +29,10 @@ export default function Home() {
         <title>Coder Space</title>
         <meta name='description' content='Blog post website for coding enthusiast ' />
       </Head>
-      <div className=' bg-gray-900 w-full h-[500px] md:h-[600px] lg:h-[600px] xl:h-[calc(100vh-300px)]'>
+      <section className=' bg-gray-900 w-full h-[500px] md:h-[600px] lg:h-[600px] xl:h-[calc(100vh-300px)]'>
         <div className='lg:container m-auto flex  h-full' >
           <div className='w-full lg:w-[70%] xxl:w-[80%] relative'>
-            <span className='p-2 block max-w-24 text-white font-bold absolute left-0 top-0 z-10'>Latest</span>
+            <span className='p-2 block max-w-24 text-white font-bold absolute left-0 top-0 z-10'>Featured</span>
             <PostSwiper />
           </div>
           <div className='hidden lg:block xl:block lg:w-[30%] xxl:w-[20%]'>
@@ -40,41 +40,16 @@ export default function Home() {
           </div>
 
         </div>
-      </div>
-      <div className='flex flex-wrap lg:container m-auto px-4 lg:p-0'>
-        <div className='bg-green-400 w-1/3 p-4 h-fit'>
+      </section>
+      <section className='flex flex-wrap lg:container m-auto px-4 lg:px-0 py-10'>
+        <div className='bg-green-400 w-1/3 p-4 h-fit  rounded-lg'>
           <div className='py-2'>
-            <div><h3 className='text-lg'>{blogs == null? (<>loading</>):blogs.data[0].title}</h3></div>
-            <div><p className='text-justify'>{blogs == null? (<>loading</>):blogs.data[0].content}</p></div>
-          </div>
-          <div className='py-2'>
-            <div><h3 className='text-lg'>{blogs == null? (<>loading</>):blogs.data[2].title}</h3></div>
-            <div><p className='text-justify'>{blogs == null? (<>loading</>):blogs.data[2].content}</p></div>
-          </div>
-
-        </div>
-        <div className='bg-blue-400 w-1/3 h-fit p-4'>
-          <div className='py-2'>
-            <div><h3 className='text-lg'>{blogs == null? (<>loading</>):blogs.data[0].title}</h3></div>
-            <div><p className='text-justify'>{blogs == null? (<>loading</>):blogs.data[0].content}</p></div>
-          </div>
-          <div className='py-2'>
-            <div><h3 className='text-lg'>{blogs == null? (<>loading</>):blogs.data[1].title}</h3></div>
-            <div><p className='text-justify'>{blogs == null? (<>loading</>):blogs.data[1].content}</p></div>
+            <Image  src={'/img/image1.jpg'} alt='tests' width={100} height={100} style={{width:'100%'}} className=' rounded-lg' />
+            <div><h3 className='text-2xl font-semibold py-3'>{blogs == null ? (<>loading</>) : blogs.data[0].title}</h3></div>
+            <div><p className='text-justify text-ellipsis text-wrap line-clamp-4'>{blogs == null ? (<>loading</>) : blogs.data[0].content}</p></div>
           </div>
         </div>
-        <div className='bg-red-400 w-1/3 h-fit p-4'>
-          <div className='py-2'>
-            <div><h3 className='text-lg'>{blogs == null? (<>loading</>):blogs.data[1].title}</h3></div>
-            <div><p className='text-justify'>{blogs == null? (<>loading</>):blogs.data[1].content}</p></div>
-          </div>
-          <div className='py-2'>
-            <div><h3 className='text-lg'>{blogs == null? (<>loading</>):blogs.data[2].title}</h3></div>
-            <div><p className='text-justify'>{blogs == null? (<>loading</>):blogs.data[2].content}</p></div>
-          </div>
-        </div>
-
-      </div>
+      </section>
     </>
   )
 }
